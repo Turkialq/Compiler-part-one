e81d0b9735ebe6d7b5b3d44f2ef81dd635810a95
Turki <w.sm@windowslive.com>
2022-10-19 18:41:52 +0300

    Final changes to question two

--- answerss/questiontwo.py
+++ answerss/questiontwo.py
@@ -1,56 +1,46 @@
 import json
-import sys
-
-DFA = {}
-NFA = {}
-nfa_states = []
-dfa_states = []
 
 
-def load_nfa(nfa_object):
-    global NFA
-    NFA = nfa_object
-
-
-def out_dfa():
-    global DFA
-    return (DFA)
-
-
-def get_power_set(nfa_States):
-    powerset = [[]]
-    for i in nfa_States:
-        for sub in powerset:
-            powerset = powerset+[list(sub)+[i]]
-    return powerset
+class nfa_to_dfa:
+    def __init__(self):
+        self.DFA = {}
+        self.NFA = {}
+        self.nfa_states = []
+        self.dfa_states = []
 
 
 def api_call(nfa_object):
     tempObject = json.loads(nfa_object)
-    load_nfa(tempObject)
 
-    DFA['states'] = []
-    DFA['letters'] = NFA['letters']
-    DFA['transition_function'] = []
+    convert = nfa_to_dfa()
 
-    for state in NFA['states']:
-        nfa_states.append(state)
+    convert.NFA = tempObject
+    convert.DFA['states'] = []
+    convert.DFA['letters'] = convert.NFA['letters']
+    convert.DFA['transition_function'] = []
+    convert.DFA['final_states'] = []
 
-    dfa_states = get_power_set(nfa_states)
+    for state in convert.NFA['states']:
+        convert.nfa_states.append(state)
 
-    DFA["states"] = []
+    powerset = [[]]
+    for i in convert.nfa_states:
+        for sub in powerset:
+            powerset = powerset+[list(sub)+[i]]
+
+    dfa_states = powerset
 
     for states in dfa_states:
         temp = []
         for state in states:
             temp.append(state)
-        DFA["states"].append(temp)
+        convert.DFA["states"].append(temp)
 
     for states in dfa_states:
-        for letter in NFA['letters']:
+        for letter in convert.NFA['letters']:
             q_to_next = []
             for state in states:
-                for val in NFA['transition_function']:
+                for val in convert.NFA['transition_function']:
                     start = val[0]
                     inp = val[1]
                     end = val[2]
@@ -60,14 +50,14 @@ def api_call(nfa_object):
             q_states = []
             for i in states:
                 q_states.append(i)
-            DFA['transition_function'].append([q_states, letter, q_to_next])
-    DFA['start_states'] = []
-    for state in NFA['start_states']:
-        DFA['start_states'].append([state])
+            convert.DFA['transition_function'].append(
+                [q_states, letter, q_to_next])
+    convert.DFA['start_states'] = []
+    for state in convert.NFA['start_states']:
+        convert.DFA['start_states'].append([state])
 
-    DFA['final_states'] = []
-    for states in DFA['states']:
+    for states in convert.DFA['states']:
         for state in states:
-            if state in NFA['final_states'] and states not in DFA['final_states']:
-                DFA['final_states'].append(states)
-    return out_dfa()
+            if state in convert.NFA['final_states'] and states not in convert.DFA['final_states']:
+                convert.DFA['final_states'].append(states)
+    return convert.DFA
